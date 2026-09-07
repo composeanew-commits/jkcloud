4fa6732a275cdb5aff6f9e70aaf08a6230faebb7
composeanew-commits <composeanew@gmail.com>
2026-09-08 01:32:25 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -9,8 +9,8 @@ const CACHE_NAME = "jk-shiurim-shell-v1";
 const APP_SHELL = [
   "./index.html",
   "./manifest.json",
-  "./icons/icon-192.png",
-  "./icons/icon-512.png",
+  "./icon-192.png",
+  "./icon-512.png",
 ];
 
 self.addEventListener("install", (event) => {
